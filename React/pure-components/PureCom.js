9b7890ae248abcb77a4c0c1fb6ddd2afba9a87bd
JesuJeyamani <95433671+jesu03@users.noreply.github.com>
2024-07-10 13:13:32 +0530

    Add files via upload

---
+++ React/pure-components/PureCom.js
@@ -0,0 +1,14 @@
+import React, { PureComponent } from 'react'
+
+
+export default class PureCom extends PureComponent {
+  
+    render() {
+        console.log(" vjv ");
+    return (
+      <div>
+        <div>Pure Com</div>
+      </div>
+    )
+  }
+}
